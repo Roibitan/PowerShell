edcff4db8324d1982a24b5860fb9732a2d654f91
Roi <Roibitan35@gmail.com>
2024-08-08 12:40:04 +0300

    update

--- AZ_Repo_Build_Release.ps1
+++ AZ_Repo_Build_Release.ps1
@@ -1,21 +1,23 @@
-﻿$User = "XXXXXX@XXXXXX.XXXXXX"
-$Pass = "XXXXXX"
-$tenant = "XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXXXXXXXX"
-$organization = "https://dev.azure.com/XXXXXXXXXXXX/"
-$Team_Project = "XXXXXX"
-$pat = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-$org = "XXXXXXXXXXXX"
+﻿$User = "XXXXXXX@XXXXXXX.XXXXXXX.com"
+$Pass = "XXXXXXX"
+$tenant = "XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX"
+$organization = "https://XXXXXXX.XXXXXXX.XXXXXXX/XXXXXXXXXXXXXX/"
+$Team_Project = "XXXXXXX"
+$pat = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+$org = "XXXXXXXXXXXXXX"
 $source_branch = "refs/heads/Dev"
 $hex_release = "Release"
 $directoryPath_todelete = "D:\Scripts\QA_Dev_Branch\"
 $filePatterns = @(
     "Branch_Create_*.txt",
     "Build_completed_*.txt",
-    "Build_started_*.txt"
+    "Build_started_*.txt",
+    "Release_completed_*.txt",
+    "test.csv"
 )
-############
-$Target_stage = "QA"
-$NewBranchName = "refs/heads/Release/Release_154"
+############ 
+$Target_stage = "QA-RC"
+$NewBranchName = "refs/heads/Release/Release_155"
 ############
 $hex_new_full = $NewBranchName.Split("/")
 $hex_new = $hex_new_full[3]
@@ -24,12 +26,13 @@ $Branch_Number = $hex_new -replace '[^\d]+', ''
 $output_file = "D:\Scripts\QA_Dev_Branch\Branch_Create_"+$Branch_Number+".txt"
 $output_file_Build = "D:\Scripts\QA_Dev_Branch\Build_started_"+$Branch_Number+".txt"
 $output_file_Build_completed = "D:\Scripts\QA_Dev_Branch\Build_completed_"+$Branch_Number+".txt"
-$policy_group = 'repo2'
-$Approvers_grp_A = "[XXXXXX XXXXXX]\XXXXXXXXXXXX"
-$infosec_grp = "[XXXXXX XXXXXX]\XXXXXX XXXXXX"
+$output_file_releases = "D:\Scripts\QA_Dev_Branch\Release_completed_"+$Branch_Number+".txt"
+$policy_group = 'XXXXXXXXXXXXXX'
+$Approvers_grp_A = "[XXXXXXX]\XXXXXXXXXXXXXX"
+$infosec_grp = "[XXXXXXX]\XXXXXXX"
 
 
-$DB = @('XXXXXX.XXXXXX.DB', 'XXXXXX.XXXXXX.DB.XXXXXX', 'XXXXXX.XXXXXX.DB.XXXXXX', 'repo2')
+$DB = @('XXXXXXX', 'XXXXXXXXXXXXXX', 'XXXXXXXXXXXXXX')
 
 
 
@@ -40,30 +43,47 @@ function Get-BuildStatus {
         [string]$Build_run_ID
     )
     #az pipelines build show --id $Build_run_ID --output table
-    # Command to get the build status
     $BuildStatus = az pipelines build show --id $Build_run_ID --output json | ConvertFrom-Json
-
     while ($BuildStatus.status -ne "completed" -and $BuildStatus.status -ne "cancelling") {
-        Start-Sleep -Seconds 3 
+        Start-Sleep -Seconds 5 
         $BuildStatus = az pipelines build show --id $Build_run_ID --output json | ConvertFrom-Json
-        Write-Host 'status is "inProgress" cheking again in 5 secent'
+        $BS = $BuildStatus.status
+        Write-Host "status is $BS cheking again in 5 secent"
     }
-
     return $BuildStatus
 }
 
+function Get-ReleaseStatus {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$ID
+    )
+    $stages = az pipelines release show --id $ID --query "environments[].{id: id, name: name, Status: status}" --output json | ConvertFrom-Json
+    foreach ($stage in $stages) {
+        if ($stage.name -ieq $Target_stage) {
+            $ReleaseStatus = $stage.Status
+            while ($ReleaseStatus -ne "succeeded" -and $ReleaseStatus -ne "Failed" -and $ReleaseStatus -ne "Canceled" -and $ReleaseStatus -ne "Rejected") {
+                Start-Sleep -Seconds 5 
+                $stages = az pipelines release show --id $ID --query "environments[].{id: id, name: name, Status: status}" --output json | ConvertFrom-Json
+                foreach ($stage in $stages) {
+                    if ($stage.name -ieq $Target_stage) {
+                        Write-Host "status is $ReleaseStatus checking again in 5 seconds"
+                        $ReleaseStatus = $stage.Status
+                    }
+                }
+            }
+            return $ReleaseStatus
+        }
+    }
+}
 
 foreach ($pattern in $filePatterns) {
-    # Get the list of files matching the current pattern
-    $files = Get-ChildItem -Path $directoryPath_todelete -Filter $pattern
-    
-    # Loop through each file and delete it
+    $files = Get-ChildItem -Path $directoryPath_todelete -Filter $pattern    
     foreach ($file in $files) {
         Remove-Item -Path $file.FullName -Force
         Write-Host "Deleted file: $($file.FullName)"
     }
 }
-
 Write-Host "File deletion completed."
 
 $BuildStatus_reset = "None"
@@ -72,17 +92,17 @@ $BuildResult_reset = "None"
 $MS_List = "D:\Scripts\QA_Dev_Branch\MicroServices.txt"
 $Content = Get-Content -Path $MS_List
 
-az login -u $User -p $Pass -t $tenant 
+#### MAIN BEGIN ####
+
+az login -u $User -p $Pass -t $tenant --output none
 az devops configure --defaults organization=$organization project=$Team_Project 
 
-#### MAIN BEGIN ####
 
 ## Create Branch in repo ##
 foreach ($repository in $Content) {
     $Branchs = az repos ref list --project $Team_Project --repository $repository --query "[].{id: objectId, name: name}" --output json | ConvertFrom-Json
     #az repos ref list --project $Team_Project --repository $repository --query "[].{id: objectId, name: name}" --output table
     foreach ($Branch in $Branchs) {
-        #Write-Host "Repository Name: $($Branch.Name)"
         if( $Branch.name -ieq $source_branch){
             Write-Host "################"
             Write-Host    $repository
@@ -93,7 +113,6 @@ foreach ($repository in $Content) {
                     $ReleaseBranchID = $obj.objectId
                 }
             }
-
             $reposlist = az repos list --project $Team_Project --query "[].{Name: name, ID: id}" --output json
             $repos = $reposlist | Out-String | ConvertFrom-Json
             $e = $ErrorActionPreference
@@ -103,7 +122,6 @@ foreach ($repository in $Content) {
                     $repository_ID = $repo.ID   
                     try {
                         $policyConfigurations_az = az repos policy list --org $organization --project $Team_Project --repository-id $repository_ID --branch $source_branch --output json
-                        #az repos policy list --organization $organization --project $Team_Project --repository-id $repository_ID --branch $source_branch --output json
                         $policyConfigurations = $policyConfigurations_az | Out-String | ConvertFrom-Json
                         $jsonData_az = az repos policy list --branch $source_branch --repository-id $repository_ID --output json 
                         $jsonData = $jsonData_az | Out-String | ConvertFrom-Json
@@ -112,7 +130,6 @@ foreach ($repository in $Content) {
                         az repos ref create --name $NewBranchName -p $Team_Project -r $repository --object-id $ReleaseBranchID
                         $Crt_Branch_Exit_Code = $LASTEXITCODE
                         write-host "##[section] Finish Create $hex_new Branch" 
-                        #pause
                         write-host "##[debug] Creating Policies"
                         if ( $repository -ieq $policy_group ){
                             az repos policy required-reviewer create --blocking true --enabled true --branch $NewBranchName --repository-id $repository_ID --required-reviewer-ids $Approvers_grp_A --message $Approvers_grp_A
@@ -125,22 +142,17 @@ foreach ($repository in $Content) {
                             $body | Set-Content -Path C:\Temp\Branch_Policies.json
                             $jsonFilePath = "C:\Temp\Branch_Policies.json"
                             $jsonContent = Get-Content -Path $jsonFilePath -Raw
-                            #$findString = "refs/heads/Dev"
-                            #$replaceString = "refs/heads/Release_154"
                             $modifiedContent = $jsonContent -replace [regex]::Escape($source_branch), $NewBranchName
                             #####################################
                             Set-Content -Path $jsonFilePath -Value $modifiedContent
                             Write-Output "String replacement complete."
                             az repos policy create --config C:\Temp\Branch_Policies.json #--output none
                         }
-                            
                             #########################
-
                             write-host "##[section] Finish Creating Policies" 
                             Write-Host "------------------------------------"
                             Write-Host "Branch '$NewBranchName' created successfully."
                             Write-Host "------------------------------------"
-                            
                         }
                         Add-Content -Path $output_file -Value \"Branch in $repository created successfully.\"
                         #############################################################################################################
@@ -149,7 +161,6 @@ foreach ($repository in $Content) {
                             if( $repository -ieq $DB_NAME){
                                 $tfs_project_info = az devops project show --project $Team_Project --org $organization --output json | ConvertFrom-Json
                                 $tfs_project_id = $tfs_project_info.id
-                                #$tfs_project_id   
                                 $repo = az repos list --query '[].{Name:name, Url:remoteUrl, ID:id}' --output json | ConvertFrom-Json
                                 foreach ($repo_all in $repo) {
                                     if( $repo_all.name -ieq $repository){
@@ -177,7 +188,7 @@ foreach ($repository in $Content) {
                                 #az devops security group list --org $organization --project $Team_Project --output table
                                 $security_groups = $security_groups.PsObject.Properties.value
                                 foreach ($security_group in $security_groups) {
-                                    if( $security_group.principalName -ieq "[TEAM FOUNDATION]\ORG-TECH-DBA PRODUCTION TEAM"){
+                                    if( $security_group.principalName -ieq "[XXXXXXX]\XXXXXXXXXXXXXX"){
                                         $subject = $security_group.descriptor
                                     }
                                 }
@@ -206,7 +217,6 @@ foreach ($repository in $Content) {
     if ( $Crt_Branch_Exit_Code -ieq "0" ){
         ## Triger build for the new Branch ##
         try {
-            #Start-Sleep -Seconds 10
             az pipelines run --name $repository --branch $NewBranchName
             Write-Host "------------------------------------"
             Write-Host "Build '$repository' Started successfully."
@@ -231,15 +241,17 @@ foreach ($repository in $Content) {
         }
         az pipelines build show --id $Build_run_ID --output table
         $BuildStatus = Get-BuildStatus -Build_run_ID $Build_run_ID
-        Add-Content -Path $output_file_Build_completed -Value \"Build * $repository * Status - $Build_Status | Result - $Build_Result\"
+        $Build_Report_Status = $BuildStatus.status
+        $Build_Report_Result = $BuildStatus.Result
+        $Rep = $repo.name
+        Write-Host "The Build $Rep status is $Build_Report_Status with Result $Build_Report_Result"
+        Add-Content -Path $output_file_Build_completed -Value \"Build * $repository * Status - $Build_Report_Status | Result - $Build_Report_Result\"
     }
     else {
         Write-Host "Skipping Build Stage"
     }
-
     $BuildStatus_reset = $BuildStatus.status
     $BuildResult_reset = $BuildStatus.Result
-
     ## Triger Release from Build ##
     sleep 5
     if (  $BuildStatus_reset -ieq "completed" -and $BuildResult_reset -ieq "succeeded" ){
@@ -250,14 +262,11 @@ foreach ($repository in $Content) {
                 $DEFINITION_ID = $obj.id
                 Write-Host "The Release is:"
                 $obj.name
-                #$DEFINITION_ID 8
+                #$DEFINITION_ID 8           
                 #az pipelines runs list --branch $NewBranchName --pipeline-ids $Build_ID -p $Team_Project --output table
                 #az pipelines run --name $repository --branch $NewBranchName --output table
                 $Release_info = "_"+$obj.name+"="+$Build_Run_ID
                 az pipelines release create --definition-name $obj.name --artifact-metadata-list "$Release_info"
-                #az pipelines release create --definition-name $obj.name --artifact-metadata-list _Repo2=268164
-                #Write-Host "sleep 10 sec..."
-                #Start-Sleep -Seconds 10
                 $RELEASE_ID_Source = az pipelines release list --source-branch $NewBranchName --org $organization --top 1 --project $Team_Project --definition-id $DEFINITION_ID --output json | ConvertFrom-Json
                 #az pipelines release list --source-branch $NewBranchName --org $organization --top 1 --project $Team_Project --definition-id $DEFINITION_ID --output table
                 $Status = "continue" 
@@ -283,12 +292,9 @@ foreach ($repository in $Content) {
                 
                             $body = @{
                                 status = "inProgress"
-                            } | ConvertTo-Json
-                            #Write-Host "sleep:10"
-                            #sleep 10
+                            } | ConvertTo-Json                                                        
                             Invoke-RestMethod -Uri $uri -Method patch -Headers @{Authorization=("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)")))} -ContentType "application/json" -Body $body
                             $Status = "Exit"  
-              
                         }
                     }
                 }
@@ -297,6 +303,63 @@ foreach ($repository in $Content) {
     }
     else {
         Write-Host "Skipping Release Stage"
+    }
+    $csvPath = "D:\Scripts\QA_Dev_Branch\test.csv"
+    $CSV_File = "$repository,$RELEASE_ID"
+    $CSV_File | Add-Content -Path $csvPath
+}
+
+$note = @"
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+############           code process is over                                                #############
+############                     begin with check release process status                   #############
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+############                                                                               #############
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
+"@
+
+Write-Host $note
+
+Get-Content -Path $csvPath | ForEach-Object {
+    $line = $_
+    $columns = $line -split ","
+    $repo = $columns[0]
+    $ID = $columns[1]
+    $stages = az pipelines release show --id $ID --query "environments[].{id: id, name: name, Status: status}" --output json | ConvertFrom-Json
+    foreach ($stage in $stages) {
+        if ($stage.name -ieq $Target_stage ) {
+            if ($stage.Status -ieq "succeeded" -or $stage.Status -ieq "Failed" -or $stage.Status -ieq "Canceled" -or $stage.Status -ieq "Rejected" ){
+                $ReleaseStatus = $stage.Status
+                Write-Host "The Release $repo status is $ReleaseStatus"
+                Add-Content -Path $output_file_releases -Value \"Release $repo status is $ReleaseStatus.\"
+            }
+            else {
+                $repo
+                $stage.Status
+                $ReleaseStatus = "Test"
+                $ReleaseStatus = Get-ReleaseStatus -ID $ID
+                Write-Host "The Release $repo status is $ReleaseStatus"
+                Add-Content -Path $output_file_releases -Value \"Release $repo status is $ReleaseStatus.\"
+            }
+        }
     }
 }
 
